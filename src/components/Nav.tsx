99ade3c8045ee2f8f0b3845ecc8f7fd4cd6f3fd4
Sebastian Casadevall <sacasadevall@gmail.com>
2024-11-28 03:42:46 -0500

    Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,24 @@
-const Nav = () => {
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../index.css';
+
+const Nav: React.FC = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <nav className='nav'>
+      <ul>
+        <li className='nav-item'>
+          <Link to="/" className='nav-link'>
+          Candidate Search
+          </Link>
+        </li>
+        <li className='nav-item'>
+          <Link to="/saved" className='nav-link'>
+          Saved Candidates
+          </Link>
+        </li>
+      </ul>
+    </nav>
   )
 };
 
